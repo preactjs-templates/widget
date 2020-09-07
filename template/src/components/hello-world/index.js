0926da6285420c3100d272f342cc0cc9a35da5fa
Ryan Christian <ryanchristian4427@gmail.com>
2020-09-06 21:51:30 -0500

    style(spacing): Correcting spacing inconsistencies

--- template/src/components/hello-world/index.js
+++ template/src/components/hello-world/index.js
@@ -2,9 +2,9 @@ import { h } from "preact";
 import "./style.css";
 
 export default function App(props) {
-    return (
-        <div>
-            <h1 style={{ color: props.color }}>Hello, World!</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1 style={{ color: props.color }}>Hello, World!</h1>
+    </div>
+  );
 }
